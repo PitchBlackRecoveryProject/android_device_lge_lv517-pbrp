d8644d8c8a30516a47b9156801e896033df27147
travisholt92 <Travisholt92@gmail.com>
2018-10-26 02:34:28 -0400

    Update to support Treble enabled builds

--- full_lv517.mk
+++ full_lv517.mk
@@ -24,6 +24,14 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+# Encryption
+PRODUCT_PACKAGES += \
+    libcryptfs_hw
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lv517
 PRODUCT_NAME := full_lv517
